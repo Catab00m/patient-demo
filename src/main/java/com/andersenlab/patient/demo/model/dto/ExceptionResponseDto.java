ef0a0de8292aa1d9966ccbd76a9a81fdb3412d3f
Artem Astakhov <fistax@yandex.ru>
2024-01-23 15:55:15 +0400

    added examples for ExceptionResponseDto

--- src/main/java/com/andersenlab/patient/demo/model/dto/ExceptionResponseDto.java
+++ src/main/java/com/andersenlab/patient/demo/model/dto/ExceptionResponseDto.java
@@ -1,5 +1,6 @@
 package com.andersenlab.patient.demo.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.OffsetDateTime;
@@ -7,12 +8,16 @@ import java.time.OffsetDateTime;
 @Builder
 public record ExceptionResponseDto(
 
+        @Schema(name = "statusCode", description = "Http Status Code", example = "404")
         int statusCode,
 
+        @Schema(name = "message", description = "Exception message", example = "Not Found")
         String message,
 
+        @Schema(name = "exceptionMessage", description = "Detailed exception message", example = "Patient with id '1' not found")
         String exceptionMessage,
 
+        @Schema(name = "timestamp", description = "Exception timestamp", example = "2024-01-01T00:00:00.123Z")
         OffsetDateTime timestamp
 ) {
 }
